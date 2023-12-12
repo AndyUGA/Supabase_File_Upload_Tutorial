f487b147e82e881f8adbd105f4a7043f719c8ae8
anton <antonjollant@aol.com>
2023-12-12 16:34:58 +0100

    deleted accidental prettier formatting

--- src/App.js
+++ src/App.js
@@ -1,105 +1,121 @@
-import { Auth } from "@supabase/auth-ui-react";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import "./App.css";
+import { Auth } from '@supabase/auth-ui-react'
+import {
+  ThemeSupa,
+} from '@supabase/auth-ui-shared'
+import './App.css';
 
 import { useEffect, useState } from "react";
 
-import { createClient } from "@supabase/supabase-js";
-import { v4 as uuidv4 } from "uuid";
+import { createClient } from '@supabase/supabase-js'
+import { v4 as uuidv4 } from 'uuid';
+
 
 const supabase = createClient(
-    process.env.REACT_APP_SUPABASE_URL,
-    process.env.REACT_APP_ANON_KEY
-);
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_ANON_KEY,
+)
 
 function App() {
-    const [userId, setUserId] = useState("");
-    const [media, setMedia] = useState([]);
 
-    const getUser = async () => {
-        try {
-            const {
-                data: { user },
-            } = await supabase.auth.getUser();
-            if (user !== null) {
-                setUserId(user.id);
-            } else {
-                setUserId("");
-            }
-        } catch (e) {}
-    };
+  const [userId, setUserId] = useState('');
+  const [media, setMedia] = useState([]);
 
-    async function uploadImage(e) {
-        let file = e.target.files[0];
+  const getUser = async () => {
 
-        const { data, error } = await supabase.storage
-            .from("uploads")
-            .upload(userId + "/" + uuidv4(), file);
-
-        if (data) {
-            getMedia();
-        } else {
-            console.log(error);
-        }
+    try {
+      const { data: { user } } = await supabase.auth.getUser()
+      if (user !== null) {
+        setUserId(user.id);
+      } else {
+        setUserId('');
+      }
+    } catch (e) {
     }
 
-    async function getMedia() {
-        const { data, error } = await supabase.storage
-            .from("uploads")
-            .list(userId + "/", {
-                limit: 10,
-                offset: 0,
-                sortBy: {
-                    column: "name",
-                    order: "asc",
-                },
-            });
 
-        if (data) {
-            setMedia(data);
-        } else {
-            console.log(71, error);
-        }
+
+  }
+
+
+  async function uploadImage(e) {
+    let file = e.target.files[0];
+
+
+    const { data, error } = await supabase
+      .storage
+      .from('uploads')
+      .upload(userId + "/" + uuidv4(), file)
+
+    if (data) {
+      getMedia();
+
+    } else {
+      console.log(error);
     }
+  }
 
-    const signout = async () => {
-        setUserId("");
-        await supabase.auth.signOut();
-    };
+  async function getMedia() {
 
-    useEffect(() => {
-        getUser();
-        getMedia();
-    }, [userId]);
+    const { data, error } = await supabase.storage.from('uploads').list(userId + '/', {
+      limit: 10,
+      offset: 0,
+      sortBy: {
+        column: 'name', order:
+          'asc'
+      }
+    });
 
-    return (
-        <div className="mt-5">
-            {userId == "" ? (
-                <Auth
-                    supabaseClient={supabase}
-                    appearance={{ theme: ThemeSupa }}
-                />
-            ) : (
-                <>
-                    <input type="file" onChange={(e) => uploadImage(e)} />
-                    <div className="mt-5">My Uploads</div>
+    if (data) {
+      setMedia(data);
+    } else {
+      console.log(71, error);
+    }
+  }
 
-                    {media.map((media) => {
-                        return (
-                            <div key={media.name}>
-                                <img
-                                    src={`https://ovlhvogwndcqxaskukrv.supabase.co/storage/v1/object/public/uploads/${userId}/${media.name}`}
-                                />
-                            </div>
-                        );
-                    })}
-                    <div className="mt-5">
-                        <button onClick={signout}>Logout</button>
-                    </div>
-                </>
-            )}
+
+  const signout = async () => {
+    setUserId('');
+    await supabase.auth.signOut();
+  }
+
+
+
+
+  useEffect(() => {
+    getUser();
+    getMedia();
+  }, [userId])
+
+  return (
+    <div className='mt-5'>
+      {userId == '' ? <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+      /> : <>
+
+        <input type="file" onChange={(e) => uploadImage(e)} />
+        <div className='mt-5'>
+          My Uploads
         </div>
-    );
+
+        {media.map((media) => {
+          return (
+            <div key={media.name}>
+                <img
+                    src={`https://ovlhvogwndcqxaskukrv.supabase.co/storage/v1/object/public/uploads/${userId}/${media.name}`}
+                />
+            </div>
+        );
+        })}
+        <div className='mt-5'>
+          <button onClick={signout}>
+            Logout
+          </button>
+        </div>
+      </>}
+    </div >
+  )
 }
 
-export default App;
+
+
